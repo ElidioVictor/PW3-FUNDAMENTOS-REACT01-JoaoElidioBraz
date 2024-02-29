9c16739bf014246562d5e4be7499e334665034c6
joao.elidio <victor.elidio00@gmail.com>
2024-02-29 20:25:51 -0300

    usando JSON para renderização dinamica por JoaoElidio

--- fundamentos-react01/src/App.js
+++ fundamentos-react01/src/App.js
@@ -1,39 +1,11 @@
-import logo from './logo.svg';
+import dadosJSON from './dados.json'
 import './App.css';
 import Livros from './components/Livro';
-import cavernas from './livros/cavernas_aco.jpg';
-import solDesvelado from './livros/sol_desvelado.jpg'
-import robosDaAlvoarda from './livros/robos_alvorada.jpg'
-
-const descr = "As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais. A obra foi publicada, pela primeira vez, na revista Galaxy Science Fiction, e o sucesso foi tão grande que, um ano depois, se tornou livro. Na trama, Os Mundos Siderais, antigas colônias da Terra, possuem rígidas leis de imigração, e não aceitam mais terráqueos em seus planetas. Para agravar a situação, os robôs estão substituindo grande parte da mão de obra trabalhadora na Terra, levando os humanos a perderem seus empregos e privilégios de cidadãos."
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Livros
-          titulo="As cavernas de aço"
-          autor="Isaac Azimov"
-          imagem={cavernas}
-          descricao={descr}
-        />
-
-        <Livros
-          titulo="Robos da alvorada"
-          autor="Isaac Azimov"
-          imagem={robosDaAlvoarda}
-          descricao={descr}
-        />
-
-        <Livros
-          titulo="Sol desvelado"
-          autor="Isaac Azimov"
-          imagem={solDesvelado}
-          descricao={descr}
-        />
-
-
-      </div>
+        <Livros dadosJSON={dadosJSON}/>
     </div>
   );
 };
--- fundamentos-react01/src/components/Livro.js
+++ fundamentos-react01/src/components/Livro.js
@@ -1,16 +1,19 @@
-
 import '../App.css';
 
-function Livros({titulo, autor, imagem, descricao}) /*chaves são necessárias para referenciar JS (java script)*/ {
+function Livros({dadosJSON}) /*chaves são necessárias para referenciar JS (java script)*/ {
     return(
-        <div>
-            <div className="cardBook">
-                <h3>{titulo}</h3>
-                <p>{autor}</p>
-                <img src={imagem} alt="Capa do livro" width={200} ></img>
-                <p>{descricao}</p>
-            </div>
-        </div>
+        <>
+            {
+                dadosJSON.map((livro) =>(
+                    <div className="cardBook">
+                        <h3>{livro.titulo}</h3>
+                        <p>{livro.autor}</p>
+                        <img src={livro.imagem} alt="Capa do livro" width={200} ></img>
+                        <p>{livro.descricao}</p>
+                    </div>
+                ) )
+            }
+        </>
     );
 };
 
